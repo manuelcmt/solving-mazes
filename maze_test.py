437d58a0c89bd42e6361a7b84dc0f4084dfb1939
Manuel Crisólogo <manuelcrisologo@hotmail.com>
2021-02-15 11:28:37 -0700

    Restauración de maze_test

---
+++ maze_test.py
@@ -0,0 +1,21 @@
+from maze import Maze
+import pytest
+
+@pytest.mark.parametrize("maze, goal",
+                            [("maze1.txt", (0, 11)),
+                             ("maze2.txt", (3, 3)),
+                             ("maze3.txt", (0, 5)),
+                             ("maze4.txt", (8, 13)),
+                             ("maze5.txt", (2, 1)),
+                             ("maze6.txt", (3, 2)),
+                             ("maze7.txt", (4, 8)),
+                            ])
+
+
+def test_meta(maze, goal):
+    m = Maze(maze)
+    m.solve
+    print(m.goal)
+    assert m.goal == goal
+
+# Test for test edit
